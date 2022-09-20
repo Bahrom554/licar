2686942080f161bd732dfccbbe55aeac84fe1217
Bahrom554 <bahrombozorboev554@gmail.com>
2022-09-20 13:24:52 +0500

    add userseed

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -14,12 +14,12 @@ use Faker\Generator as Faker;
 |
 */
 
-$factory->define(App\User::class, function (Faker $faker) {
+ $factory->define(App\User::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
+        'email' => 'licar@gmail.com',
         'email_verified_at' => now(),
-        'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
+        'password' =>bcrypt('licar22'),
         'remember_token' => Str::random(10),
     ];
 });
